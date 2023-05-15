778c09fa93c250184c7e473242c71628fe478ccb
Mike Wood <mikewwood@icloud.com>
2023-05-15 16:27:20 -0400

    Feature: add insert method.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -14,9 +14,7 @@ class LinkedList
       @head = new_node
     else
       current_node = @head
-      while current_node.next_node != nil
-        current_node = current_node.next_node
-      end
+      current_node = current_node.next_node until current_node.next_node.nil?
       current_node.next_node = new_node
     end
   end
@@ -28,6 +26,22 @@ class LinkedList
     else
       new_node.next_node = @head
       @head = new_node
+    end
+  end
+
+  def insert(data, position)
+    new_node = Node.new(data)
+    if position == 0
+      new_node.next_node = @head
+      @head = new_node
+    else
+      current_node = @head
+      (position - 1).times do
+        current_node = current_node.next_node
+        raise 'Invalid position' if current_node.nil?
+      end
+      new_node.next_node = current_node.next_node
+      current_node.next_node = new_node
     end
   end
 
