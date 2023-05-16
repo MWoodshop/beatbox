78ca6898c6c5fc584e298378ff98d39ed55fe898
Mike Wood <mikewwood@icloud.com>
2023-05-15 22:07:09 -0400

    Feature: adds prepend method and BEATS_LIST

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,15 +4,43 @@ require './lib/linked_list'
 class BeatBox
   attr_reader :list
 
-  def initialize
+  BEATS_LIST = %w[deep doo ditt woo hoo shu tee dee bop la na]
+
+  def initialize(starting_word = nil)
     @list = LinkedList.new
+    append(starting_word) if starting_word
   end
 
   def append(data)
+    return if data.nil?
+
     words = data.split(' ')
     words.each do |word|
-      @list.append(word)
+      if BEATS_LIST.include?(word)
+        @list.append(word)
+      else
+        puts "Invalid beat: #{word}. Skipping."
+        return
+      end
     end
+  end
+
+  def prepend(data)
+    return if data.nil?
+
+    words = data.split(' ')
+    invalid_words = []
+    words.reverse.each do |word|
+      if BEATS_LIST.include?(word)
+        @list.prepend(word)
+      else
+        invalid_words << word
+      end
+    end
+
+    return unless invalid_words.any?
+
+    puts "Invalid beats: #{invalid_words.join(', ')}. Skipping."
   end
 
   def count
@@ -24,11 +52,15 @@ class BeatBox
     current_node = @list.head
     until current_node.nil?
       output += "Playing #{current_node.data}...\n"
-      system("say #{current_node.data}")
+      system("say -r 500 -v Samantha #{current_node.data}")
       output += "#{current_node.data} played.\n"
       current_node = current_node.next_node
     end
     puts output # for debugging purposes
     output
+  end
+
+  def all
+    @list.to_string
   end
 end
